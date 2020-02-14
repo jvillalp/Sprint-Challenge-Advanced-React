18c60b299b69ff04fa29f29c1b505d410c3a1723
Juana Villalpando <juana.v14@gmail.com>
2020-02-14 08:50:31 -0800

    add class component, run axios call, created js fles

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import axios from 'axios';
 
-function App() {
+import './App.css';
+import WomensCard from './Components/WomensCard';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      players:[]
+    }
+  }
+
+  componentDidMount(){
+    axios
+        .get('http://localhost:5200/api/players')
+        .then(res =>{
+          console.log("women world cup players",res.data);
+          this.setState({
+            players: res.data
+          })
+          })
+          .catch(err =>{
+            console.log("This is an error on search", err);
+        })
+  }
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Women's Soccer</h1>
+      {this.state.players.map(player =>(
+        <WomensCard key={player.id} player={player} />
+      ))}
     </div>
   );
 }
-
+}
 export default App;
